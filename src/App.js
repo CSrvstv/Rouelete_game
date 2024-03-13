e200c87cc2760a06cb12f62688002c8e4320cb30
chetna srivastava <chetnasrivastava@chetnas-MacBook-Air.local>
2024-03-13 23:56:22 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Start from  "./components/Start";
+import Game from './components/Game';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <Start/>
-
-
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Start.js
+++ src/components/Start.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
+
 
 export default function Start() {
   return (
-    <button onClick={()=>{}}>START</button>
-    
+    <Link to="/Game">
+    <button>START</button>
+    </Link>
   )
 }
